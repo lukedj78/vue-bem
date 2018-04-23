62356518d98405d67110d270a5fa3e535349e0c2
Luca Di Gerlando <luca.digerlando@alidays.it>
2018-04-23 10:25:59 +0200

    Fix

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,41 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,body{height:100%;}
+body{background: #eee;}
+
+/*-----------------------------
+general
+-----------------------------*/
+
+.animate{
+	-webkit-transition: all 0.3s ease-out;
+					transition: all 0.3s ease-out;
+}
+
+.button{display: inline-block; background: #fff; color:#000;padding: 15px 30px; border-radius:3px; text-decoration: none;}
+p{line-height: 22px}
+a{color:#16dd94}
+a:hover{color: #1af3a4;}
+
+/* clearfix */
+.clearfix:after {
+	visibility: hidden;
+	display: block;
+	font-size: 0;
+	content: " ";
+	clear: both;
+	height: 0;
+	}
+
+* html .clearfix             { zoom: 1; } /* IE6 */
+*:first-child+html .clearfix { zoom: 1; } /* IE7 */
+
+*,
+*:before,
+*:after {
+  -webkit-box-sizing: border-box;
+     -moz-box-sizing: border-box;
+          box-sizing: border-box;
 }
 </style>
